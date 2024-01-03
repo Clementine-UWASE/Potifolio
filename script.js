b4a831839cf26f5c76b223cb5d8d62da61eb77b2
UWASE Clementine <clemawase@gmail.com>
2024-01-03 04:00:21 +0200

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed("#career", {
+    strings: ["Front-End Developer ", "UI/UX Designer", "IoT Technician","& IT Engineer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
